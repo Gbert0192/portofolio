3764e9fccf52be1c0e173989d331402a8501da66
gilbert <gilbert.t@adaremit.co.id>
2025-03-28 15:49:00 +0700

    didnt done yet

---
+++ src/components/about-me/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
+
+const AboutMe = () => {
+  return (
+    <div className="flex items-center min-h-[calc(100vh-5rem)] max-w-[70vw] justify-center mx-auto">
+      <div className="flex flex-row items-center gap-9">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2, ease: "easeOut" }}
+        >
+          <Image
+            src="/icebear.jpg"
+            alt="main-page-picture"
+            width={400}
+            height={400}
+            className="rounded-lg max-w-[400px] w-full h-auto min-w-[400px]"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+        >
+          <div className="flex flex-col gap-2 ">
+            <h1 className="text-6xl">
+              <strong>Hellow.. I am Gilbert Tiofandy,</strong>
+            </h1>
+            <div className="max-w-150">
+              <Typewriter
+                words={[
+                  "I'm a Computer Science student. I have a strong passion for programming and have been learning and working with various programming languages and technologies. I am always eager to learn and explore new things in the world of technology.\nI like watching Korean drama and doing some coding projects.",
+                ]}
+                loop={false}
+                typeSpeed={10}
+                cursor
+                cursorStyle="|"
+                cursorBlinking
+                delaySpeed={999999999}
+              />
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,41 +3,66 @@ import React, { useState } from "react";
 import { ThemeToggle } from "../themeToggle";
 import Link from "next/link";
 import Hamburger from "../hamburger";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex items-center justify-between flex-row min-h-20">
       <div className="text-[20px] font-bold m-5 ml-9 lg:text-2xl flex-1/2">
-        Gilbert Tiofandy
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+        >
+          Gilbert Tiofandy
+        </motion.div>
       </div>
 
-      <div className="flex-row justify-center gap-9 m-5 font-semibold md:flex hidden flex-2/3 pt-1">
-        <Link
-          href="#"
-          className="relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 after:ease-in-out hover:after:w-full dark:after:bg-white"
-        >
-          Contact
-        </Link>
-        <Link
-          href="#"
-          className="relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 after:ease-in-out hover:after:w-full dark:after:bg-white"
-        >
-          About Me
-        </Link>
-        <Link
-          href="#"
-          className="relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 after:ease-in-out hover:after:w-full dark:after:bg-white"
-        >
-          Project
-        </Link>
-      </div>
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: "linear" }}
+      >
+        <div className="flex-row justify-center gap-9 m-5 font-semibold md:flex hidden flex-2/3 pt-1">
+          <Link
+            href="#"
+            className="relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 after:ease-in-out hover:after:w-full dark:after:bg-white"
+          >
+            Contact
+          </Link>
+          <Link
+            href="#"
+            className="relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 after:ease-in-out hover:after:w-full dark:after:bg-white whitespace-nowrap"
+          >
+            About Me
+          </Link>
+          <Link
+            href="#"
+            className="relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 after:ease-in-out hover:after:w-full dark:after:bg-white"
+          >
+            Project
+          </Link>
+        </div>
+      </motion.div>
 
       <div className="mr-5 flex flex-row gap-2 flex-1/2 justify-end items-center">
         <div className="md:hidden">
-          <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+          >
+            <Hamburger isOpen={isOpen} setIsOpen={setIsOpen} />
+          </motion.div>
         </div>
-        <ThemeToggle />
+        <motion.div
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.7, ease: "easeOut" }}
+        >
+          <ThemeToggle />
+        </motion.div>
       </div>
     </div>
   );
